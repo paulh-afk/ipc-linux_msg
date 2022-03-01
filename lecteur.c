e1dedc7b788dc5483b44e2b4a0286a2d6c453f35
Paul <paul.helleu@orange.fr>
2022-03-01 21:48:09 +0100

    send-receive msg

--- lecteur.c
+++ lecteur.c
@@ -3,8 +3,50 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <string.h>
+
+typedef struct MSGRQ MSGRQ;
+struct MSGRQ {
+    int type;
+    char message[20];
+};
 
 int main(int argc, char* argv[]) {
+    key_t cle;
+    int msgid, type;
+    MSGRQ *monMessage = NULL;
+
+    if(argc != 2) {
+        perror("argc");
+        return EXIT_FAILURE;
+    }
+
+    if(!atoi(argv[1])) {
+        perror("argv[1]");
+        return EXIT_FAILURE;
+    }
+
+    cle = ftok("/usr/include/err.h", 0);
+    type = atoi(argv[1]);
+
+    msgid = msgget(cle, IPC_EXCL | 0600);
+    if(msgid == -1) {
+        perror("MSG Queue");
+        return EXIT_FAILURE;
+    }
+
+    monMessage = malloc(sizeof(*monMessage));
+
+    if(monMessage == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    if(msgrcv(msgid, monMessage, (sizeof(MSGRQ) - sizeof(int)), 0, IPC_NOWAIT) == -1) {
+        perror("Aucun message");
+        return EXIT_FAILURE;
+    }
+
+    printf("Type: %d\nContenu: %s\n", monMessage->type, monMessage->message);
 
     return 0;
 }
